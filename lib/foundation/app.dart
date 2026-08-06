1648cea26ceb2e924ff8d4070bf17c592959cadc
darknight226 <2653742098@qq.com>
2026-08-06 14:21:26 +0800

    chore: bump version to 1.7.4+174

--- lib/foundation/app.dart
+++ lib/foundation/app.dart
@@ -14,7 +14,7 @@ export "context.dart";
 
 class _App {
   // 必须与 pubspec.yaml 的 version 保持一致（由 test/version_consistency_test.dart 守卫）
-  final version = "1.7.3";
+  final version = "1.7.4";
 
   bool get isAndroid => Platform.isAndroid;
 
